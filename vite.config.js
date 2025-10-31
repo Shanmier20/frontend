1b67090651e7a13894b03f6bebcb1bd3e7650d90
Shanmier20 <santosmier16@gmail.com>
2025-10-31 09:13:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["discriminatively-runniest-wendi.ngrok-free.dev", "frontend-six-psi-68.vercel.app", // ✅ your live Vercel domain],
+    allowedHosts: ["discriminatively-runniest-wendi.ngrok-free.dev", "frontend-six-psi-68.vercel.app",] // ✅ your live Vercel domain],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
