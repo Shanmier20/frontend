f8300c04a8744e181052a864d9beb01a07215820
Shanmier20 <santosmier16@gmail.com>
2025-10-31 09:14:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["discriminatively-runniest-wendi.ngrok-free.dev", "frontend-six-psi-68.vercel.app",] // ✅ your live Vercel domain],
+    allowedHosts: [
+      "discriminatively-runniest-wendi.ngrok-free.dev",
+      "frontend-six-psi-68.vercel.app", // ✅ your live Vercel domain
+    ],
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3000", // ✅ Local backend for dev only
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
